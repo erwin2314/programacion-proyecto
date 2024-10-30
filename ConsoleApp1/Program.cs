da71535aaf261b5688b1c078fb5ce04e72ee665d
Erwin <carlosjjp2016@gmail.com>
2024-10-30 10:24:23 -0600

    comentario

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 {
     static void Main()
     {
+        Console.WriteLine("El ARCHIVO SE VA A CREAR DE CERO SI UNA SOLA PARTE DEL ENCABEZADO FALTA");
         if(Funciones.comprobar_archivo() == false)
         {
             Funciones.crear_archivo();
