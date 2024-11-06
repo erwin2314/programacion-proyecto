e94526c3401ca84172a8b46e88b744fd42d088ed
Erwin <58240962+erwin2314@users.noreply.github.com>
2024-11-06 12:59:30 -0600

    Update Funciones.cs

--- ConsoleApp1/Funciones.cs
+++ ConsoleApp1/Funciones.cs
@@ -62,7 +62,6 @@ class Funciones
             {
                 
                 string line = Sr.ReadLine();
-                Console.WriteLine(line);
                 if (line.Length < 4)
                 {
                     Excepciones.Lanzar_excepcion(2);
@@ -92,7 +91,6 @@ class Funciones
             }
             if (fila > 0 || fila < 10)
             {
-                Console.WriteLine(fila);
                 texto_antiguo[fila] = (fila + 1) + ";" + existencia + ";" + texto_nuevo;
             }
         }
@@ -104,22 +102,102 @@ class Funciones
             }
         }
     }
-    public static int[,] crear_matriz()
+    public static void imprimir_texto()
+    {
+        using (StreamReader Sr = new StreamReader("matrices.txt"))
+        {
+            string[] texto = new string[9];
+            string line;
+            for (int i = 0;i < 9;i++)
+            {
+                line = Sr.ReadLine();
+                Console.WriteLine(line);   
+            }
+        }
+    }
+    public static int[,] crear_matriz_temporal()
     {
         bool conversion;
         Console.WriteLine("Ingresa la cantidad de columnas");
         conversion = Int32.TryParse(Console.ReadLine(), out int columnas);
         if (conversion == false)
         {
+            Excepciones.Lanzar_excepcion(3);
+            Excepciones.Lanzar_excepcion(4);
             return null;
         }
         Console.WriteLine("Ingresa la cantidad de filas");
         conversion = Int32.TryParse(Console.ReadLine(), out int filas);
         if (conversion == false)
         {
+            Excepciones.Lanzar_excepcion(3);
+            Excepciones.Lanzar_excepcion(4);
             return null;
         }
-        int[,] matriz = new int[columnas,filas];
-        return matriz;
+        
+        if ((filas > 1) && (columnas > 1))
+        {
+            Console.WriteLine("Para ingresar cualquier valor debe ir unicamente el valor numerico");
+            int[,] matriz = new int[columnas, filas];
+            for (int i = 0; i <= columnas-1; i++)
+            {
+                for (int j = 0; j <= filas-1; j++)
+                {
+                    Console.WriteLine("Ingresa el valor de la columna: " + (i+1) + " y fila: " + (j+1));
+                    conversion = Int32.TryParse(Console.ReadLine(), out int numero);
+                    if (conversion == false)
+                    {
+                        Excepciones.Lanzar_excepcion(3);
+                        Excepciones.Lanzar_excepcion(4);
+                        return null;
+                    }
+                    
+                    matriz[i, j] = numero;
+
+                }
+            }
+            
+            return matriz;
+        }
+        Excepciones.Lanzar_excepcion(3);
+        Excepciones.Lanzar_excepcion(4);
+        return null;
+    }
+    public static int[,] guardar_matriz_temporal_programa(int[,] matriz_a_guardar, int matriz_en_la_que_guardar)
+    {
+        switch (matriz_en_la_que_guardar)
+        {
+            case 1:
+                int[,] matriz_temporal_1 = matriz_a_guardar;
+                return matriz_temporal_1;
+            case 2:
+                int[,] matriz_temporal_2 = matriz_a_guardar;
+                return matriz_temporal_2;
+            case 3:
+                int[,] matriz_temporal_3 = matriz_a_guardar;
+                return matriz_temporal_3;
+            case 4:
+                int[,] matriz_temporal_4 = matriz_a_guardar;
+                return matriz_temporal_4;
+            case 5:
+                int[,] matriz_temporal_5 = matriz_a_guardar;
+                return matriz_temporal_5;
+            case 6:
+                int[,] matriz_temporal_6 = matriz_a_guardar;
+                return matriz_temporal_6;
+            case 7:
+                int[,] matriz_temporal_7 = matriz_a_guardar;
+                return matriz_temporal_7;
+            case 8:
+                int[,] matriz_temporal_8 = matriz_a_guardar;
+                return matriz_temporal_8;
+            case 9:
+                int[,] matriz_temporal_9 = matriz_a_guardar;
+                return matriz_temporal_9;
+            default:
+                Excepciones.Lanzar_excepcion(3);
+                return null;
+        }
+
     }
 }
