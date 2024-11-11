b09109254114568e0e6d3c74b1c9a28c2accf0d1
Erwin <58240962+erwin2314@users.noreply.github.com>
2024-11-11 09:30:00 -0600

    Update Excepciones.cs

--- ConsoleApp1/Excepciones.cs
+++ ConsoleApp1/Excepciones.cs
@@ -5,25 +5,44 @@ public class Excepciones
         switch (numero_error)
         {
             case 0:
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Se encontro un problema al buscar el archivo, se va a crear uno nuevo");
+                Console.ForegroundColor = ConsoleColor.White;
                 break;
             case 1:
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Se encontro un problema al revisar el encabezado del archivo");
+                Console.ForegroundColor = ConsoleColor.White;
                 break;
             case 2:
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Se encontro un problema al revisar la existencia de la matriz en alguna posicion, la posicion se va a limpiar y esteblecer a 0");
+                Console.ForegroundColor = ConsoleColor.White;
                 break;
             case 3:
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("No se puede utilizar el valor ingresado");
+                Console.ForegroundColor = ConsoleColor.White;
                 break;
             case 4:
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("No se pudo crear la matriz");
+                Console.ForegroundColor = ConsoleColor.White;
                 break;
             case 5:
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("No se pudo guardar en el archivo");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            case 6:
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No se pudo leer la matriz");
+                Console.ForegroundColor = ConsoleColor.White;
                 break;
             case 100:
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Ocurrio un error inesperado");
+                Console.ForegroundColor = ConsoleColor.White;
                 break;
             default:
                 break;
